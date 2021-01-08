44663289be44d19195498675c24c74035bd2c5b8
Mirko Braic <mirko.braic@gmail.com>
2021-01-08 01:24:01 +0100

    Add 5 new levels

--- ChopTheTwine/Models/LevelData.swift
+++ ChopTheTwine/Models/LevelData.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 struct LevelData {
-    var crocodileLocation = CGPoint()
-    var prizeLocation = CGPoint()
+    var crocodileLocation: CGPoint!
+    var prizeLocation: CGPoint!
     var anchorLocations = [CGPoint]()
 }
